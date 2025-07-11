4bb9ebd8a7d4b7c813f5b167d965c94a7c1e6953
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2025-07-11 11:45:17 -0300

    feat: add gemini

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
@@ -8,14 +9,17 @@ import {
 import { env } from './env.ts'
 import { createQuestion } from './http/routes/create-question.ts'
 import { createRoom } from './http/routes/create-room.ts'
-import { getRoomQuestions } from './http/routes/get-room-question.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { getRooms } from './http/routes/get-rooms.ts'
+import { uploadAudio } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -28,5 +32,6 @@ app.register(getRooms)
 app.register(createRoom)
 app.register(getRoomQuestions)
 app.register(createQuestion)
+app.register(uploadAudio)
 
 app.listen({ port: env.PORT })
